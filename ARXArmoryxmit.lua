af8105ca63082a823f20f8bd613bb8900d275132
Jeff Dickson <jeff@jdsoftcode.net>
2020-08-30 13:36:16 -0700

    Universal Lua
    Will send classic NPC targets to classic.wowhead.
    Texture path is now variable (for side-by-side testing)

--- ARXArmoryxmit.lua
+++ ARXArmoryxmit.lua
@@ -11,23 +11,25 @@ SLASH_ARXARMVERBOSE1 = "/ARXArmoryverbose"	-- echo each playor or NPC sent, foll
 
 ARXArmInCalibrationMode = 0					--  flag to suspend event processing (and update user message)
 ARXArmCalCountdown = 0
+ARXArmTexturePath = "Interface\\AddOns\\ARXArmoryxmit\\"
 ARXArmNeedToPlayDoneSound = false
+JADisWoWClassic = select(4, GetBuildInfo()) < 20000
 --ARXArmoryVerboseMode	is defined as a Saved Variable in the .toc
 
 -------------------------- THE SLASH COMMANDS EXECUTE CODE HERE ------------------------
 
 SlashCmdList["ARXARMCAL"] = function(msg, theEditFrame)		--  calibrate
-	ChatFrame1:AddMessage( "Calibration pattern set. Be sure WoW is in \"Windowed (fullscreen)\" mode.")
+	ChatFrame1:AddMessage( "ARX Armory: Calibration pattern set. Be sure WoW is in \"Windowed (fullscreen)\" mode.")
 	ARXArmSetupGuardPixels()
 	ARXArmGuardPixels(0)
-	ARXArmoryxmitA1Texture:SetTexture("Interface\\AddOns\\ARXArmoryxmit\\04")	-- calibration pattern
-	ARXArmoryxmitA2Texture:SetTexture("Interface\\AddOns\\ARXArmoryxmit\\05")
-	ARXArmoryxmitA3Texture:SetTexture("Interface\\AddOns\\ARXArmoryxmit\\06")
-	ARXArmoryxmitA4Texture:SetTexture("Interface\\AddOns\\ARXArmoryxmit\\07")
-	ARXArmoryxmitA5Texture:SetTexture("Interface\\AddOns\\ARXArmoryxmit\\01")
-	ARXArmoryxmitA6Texture:SetTexture("Interface\\AddOns\\ARXArmoryxmit\\02")
-	ARXArmoryxmitA7Texture:SetTexture("Interface\\AddOns\\ARXArmoryxmit\\03")
-	ARXArmoryxmitA8Texture:SetTexture("Interface\\AddOns\\ARXArmoryxmit\\04")
+	ARXArmoryxmitA1Texture:SetTexture(ARXArmTexturePath.."04")	-- calibration pattern
+	ARXArmoryxmitA2Texture:SetTexture(ARXArmTexturePath.."05")
+	ARXArmoryxmitA3Texture:SetTexture(ARXArmTexturePath.."06")
+	ARXArmoryxmitA4Texture:SetTexture(ARXArmTexturePath.."07")
+	ARXArmoryxmitA5Texture:SetTexture(ARXArmTexturePath.."01")
+	ARXArmoryxmitA6Texture:SetTexture(ARXArmTexturePath.."02")
+	ARXArmoryxmitA7Texture:SetTexture(ARXArmTexturePath.."03")
+	ARXArmoryxmitA8Texture:SetTexture(ARXArmTexturePath.."04")
 end
 
 SlashCmdList["ARXARMVERBOSE"] = function(msg, theEditFrame)		-- control user feedback
@@ -50,24 +52,33 @@ end
 SlashCmdList["ARXARMGO"] = function(msg, theEditFrame)			-- the main routine (/ArxArmory)
 	if UnitExists("target") then
 		if (UnitIsPlayer("target")) then					-- IF THIS IS A PLAYER TARGET:
-			local targetName = GetUnitName("target", true)		-- get name & realm
-			
-			if string.find(targetName, "-") then				-- if player NOT on same realm, then realm appears after dash
-				-- do nothing, we have the realm name included
+			if JADisWoWClassic then
+				print("ARX Armory: Blizzard does not provide a WoW Armory for Classic characters.")
 			else
-				targetName = targetName.."-"..GetRealmName()  	-- append realm to local player name
+				local targetName = GetUnitName("target", true)		-- get name & realm
+			
+				if string.find(targetName, "-") then				-- if player NOT on same realm, then realm appears after dash
+					-- do nothing, we have the realm name included
+				else
+					targetName = targetName.."-"..GetRealmName()  	-- append realm to local player name
+				end
+				if ARXArmoryVerboseMode == 1 then
+					ChatFrame1:AddMessage("ARX Armory: Sending "..targetName.." to the app")
+				end
+				ARXArmSendMessage(targetName..string.char(7)..string.char(8))  		-- append ASCII 7 & 8 to indicate end of name
 			end
-			if ARXArmoryVerboseMode == 1 then
-				ChatFrame1:AddMessage("ARX Armory: Sending "..targetName.." to the app")
-			end
-			ARXArmSendMessage(targetName..string.char(7)..string.char(8))  		-- append ASCII 7 & 8 to indicate end of name
 		else												-- IF THIS IS NOT A PLAYER TARGET:
 			local guid = UnitGUID("target")
 			local type, _, _, _, _, npc_id = strsplit("-",guid) -- type can be Player, Pet, Creature, and other stuff
-			if type == "Creature" then							-- all NPCs are Creatures
-				local targetName = "NPC"..npc_id
+			if type == "Creature" then					-- all NPCs are Creatures
+				local targetName
+				if JADisWoWClassic then
+					targetName = "C"..npc_id
+				else
+					targetName = "R"..npc_id
+				end
 				if ARXArmoryVerboseMode == 1 then
-					ChatFrame1:AddMessage("ARX Armory: Sending "..targetName.." to the app")
+					ChatFrame1:AddMessage("ARX Armory: Sending NPC #"..npc_id.." to the app")
 				end
 				ARXArmSendMessage(targetName..string.char(7)..string.char(8))   -- append ASCII 7 & 8 to indicate end of name
 			else 
@@ -75,7 +86,11 @@ SlashCmdList["ARXARMGO"] = function(msg, theEditFrame)			-- the main routine (/A
 			end
 		end
 	else 
-		ChatFrame1:AddMessage("ARX Armory: Target a player or NPC.") -- NOTHING TARGETTED
+		if JADisWoWClassic then
+			print("ARX Armory: Target an NPC.")
+		else
+			ChatFrame1:AddMessage("ARX Armory: Target a player or NPC.") -- NOTHING TARGETTED
+		end
 	end
 end
 
@@ -124,9 +139,9 @@ function ARXArmoryPutMsgOnPixels(msg)
 	--print("encoding plain byte " .. theCode)
 	for i = 1,8 do
 		if bit.band(theCode,bitmask) > 0 then		-- uses C library that Blizzard included
-			_G["ARXArmoryxmitA"..i.."Texture"]:SetTexture("Interface\\AddOns\\ARXArmoryxmit\\07")
+			_G["ARXArmoryxmitA"..i.."Texture"]:SetTexture(ARXArmTexturePath.."07")
 		else
-			_G["ARXArmoryxmitA"..i.."Texture"]:SetTexture("Interface\\AddOns\\ARXArmoryxmit\\00")
+			_G["ARXArmoryxmitA"..i.."Texture"]:SetTexture(ARXArmTexturePath.."00")
 		end	
 		bitmask = bitmask * 2						-- proven faster than bit shifting
 	end
@@ -139,15 +154,15 @@ end
 
 function ARXArmGuardPixels(state)
 	if state == 0 then
-		ARXArmoryxmitR2Texture:SetTexture("Interface\\AddOns\\ARXArmoryxmit\\00")
-		ARXArmoryxmitL2Texture:SetTexture("Interface\\AddOns\\ARXArmoryxmit\\00")
+		ARXArmoryxmitR2Texture:SetTexture(ARXArmTexturePath.."00")
+		ARXArmoryxmitL2Texture:SetTexture(ARXArmTexturePath.."00")
 	else
-		ARXArmoryxmitR2Texture:SetTexture("Interface\\AddOns\\ARXArmoryxmit\\05")
-		ARXArmoryxmitL2Texture:SetTexture("Interface\\AddOns\\ARXArmoryxmit\\06")
+		ARXArmoryxmitR2Texture:SetTexture(ARXArmTexturePath.."05")
+		ARXArmoryxmitL2Texture:SetTexture(ARXArmTexturePath.."06")
 	end	
 end
 	
 function ARXArmSetupGuardPixels()		
-	ARXArmoryxmitR1Texture:SetTexture("Interface\\AddOns\\ARXArmoryxmit\\05")
-	ARXArmoryxmitL1Texture:SetTexture("Interface\\AddOns\\ARXArmoryxmit\\06")
+	ARXArmoryxmitR1Texture:SetTexture(ARXArmTexturePath.."05")
+	ARXArmoryxmitL1Texture:SetTexture(ARXArmTexturePath.."06")
 end
